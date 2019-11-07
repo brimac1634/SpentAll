c83f45b01392d1be0696c1594a465a13a3f54b7e
Brian MacPherson <brimac1634@gmail.com>
2019-11-07 20:02:38 +0800

    started to add circular menu for mobile

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import MediaQuery from 'react-responsive';
 import { Link } from 'react-router-dom';
 
 import { selectCurrentUser } from '../../redux/user/user.selectors';
@@ -10,6 +11,7 @@ import { ReactComponent as SettingsIcon } from '../../assets/settings.svg'
 import { ReactComponent as AnalyticsIcon } from '../../assets/analytics.svg'
 import { ReactComponent as ListIcon } from '../../assets/list.svg'
 import { ReactComponent as HomeIcon } from '../../assets/home.svg'
+import { ReactComponent as MenuIcon } from '../../assets/menu.svg'
 
 import './header.styles.scss';
 
@@ -17,28 +19,40 @@ const mapStateToProps = createStructuredSelector({
 	currentUser: selectCurrentUser
 })
 
-const Header = ({ currentUser }) => (
-	<div className='header'>
-		<div className='logo-container'>
-			<Link to={'/'}>
-				<Logo className='logo'/>
-			</Link>
+const Header = ({ currentUser }) => {
+	const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+	return (
+		<div className='header'>
+			<div className='logo-container'>
+				<Link to={'/'}>
+					<Logo className='logo'/>
+				</Link>
+			</div>
+			<div className={`icon-set ${currentUser ? 'show' : 'hide'}`}>
+				<Link to='/user/account' className='icon'>
+					<HomeIcon />
+				</Link>
+				<Link to='/user/account/analytics' className='icon'>
+					<AnalyticsIcon />
+				</Link>
+				<Link to='/user/account/expenditures' className='icon'>
+					<ListIcon />
+				</Link>
+				<Link to='/user/account/settings' className='icon'>
+					<SettingsIcon />
+				</Link>
+			</div>
+			<MediaQuery maxWidth={780}>
+				<div 
+					className='icon menu' 
+					onClick={()=>setMenuIsOpen(!menuIsOpen)} 
+				>
+					<MenuIcon />
+				</div>
+			</MediaQuery>
 		</div>
-		<div className={`icon-set ${currentUser ? 'show' : 'hide'}`}>
-			<Link to='/user/account' className='icon'>
-				<HomeIcon />
-			</Link>
-			<Link to='/user/account/analytics' className='icon'>
-				<AnalyticsIcon />
-			</Link>
-			<Link to='/user/account/expenditures' className='icon'>
-				<ListIcon />
-			</Link>
-			<Link to='/user/account/settings' className='icon'>
-				<SettingsIcon />
-			</Link>
-		</div>
-	</div>
-)
+	)
+}
 
 export default connect(mapStateToProps)(Header);
