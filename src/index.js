e4e15a0b9604ac73158093e9568f9b769d007bae
Brian MacPherson <brimac1634@gmail.com>
2020-01-23 22:58:20 +0800

    added scroll to top component to index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import ScrollToTop from './components/scroll-to-top/scroll-to-top.component';
 
 import './index.css';
 import App from './App';
@@ -10,6 +11,7 @@ import App from './App';
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
+			<ScrollToTop />
 			<App />
 		</BrowserRouter>
   	</Provider>
